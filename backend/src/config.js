5c588150ee132e3885241e79ca3e130fe6bb6c23
diego <20230602@ricaldone.edu.sv>
2025-03-20 15:06:17 -0600

    Subiendo mi mini ptc

---
+++ backend/src/config.js
@@ -0,0 +1,16 @@
+//Importo libreria que acabo de instalar
+import dotenv from "dotenv";
+ 
+//Ejecuto "Dotenv"
+//me ayudara a acceder al .env
+dotenv.config();
+ 
+export const config = {
+    db: {
+        URI: process.env.DB_URI
+    },
+ 
+    server: {
+        port: process.env.PORT || 4000,
+    },
+};
